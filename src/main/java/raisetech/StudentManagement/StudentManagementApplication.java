7c84defa03a222a940c7c93cb194e4a3ba5e6bf8
Akinori Hiyamizu <or9ih5um0kaz7iyi8@gmail.com>
2025-07-03 12:30:17 +0900

    privateと@PostMappingの追記

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -3,19 +3,29 @@ package raisetech.StudentManagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
+	private String name = "Taro Yamada";
+	private String age = "30";
+
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello (){
-		return "Hello World!!";
+	@GetMapping("/studentInfo")
+	public String getStudentInfo (){
+		return name + " " + age + "歳";
+	}
+
+	@PostMapping("/studentInfo")
+	public void setStudentInfo(String name,String age){
+		this.name = name;
+		this.age = age;
 	}
 
 }
